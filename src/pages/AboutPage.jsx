a8bb09495b45a7fbb70c40594ff6a3943afe251c
Hein <nwaimarn@gmail.com>
2025-01-12 17:40:57 +0700

    about page finished

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,5 +1,289 @@
 function AboutPage() {
-  return <div>AboutPage</div>;
+  return (
+    <div className="text-md sm:text-2xl">
+      <h1 className="text-4xl font-bold mb-8 text-center">💫 About Me</h1>
+      <div className="max-w-4xl mx-auto">
+        <hr className="mb-4" />
+        <h2 className="text-xl sm:text-3xl font-bold mb-4">
+          👋 Hi, I&apos;m{" "}
+          <span className="text-[var(--accent-color)]">Kaung Hset Hein</span>{" "}
+          a.k.a
+          <span className="text-[var(--accent-color)]"> PhilixTheExplorer</span>
+          !
+        </h2>
+        <p className="sm:text-lg mb-4">
+          🎓 Computer Science Student <br className="sm:hidden" />| 💻 Aspiring
+          Software Engineer <br className="sm:hidden" />| 📚 Lifelong Learner
+        </p>
+        <hr className="my-4" />
+        <h3 className="font-bold mb-4">
+          🚀{" "}
+          <span className="bg-gradient-to-r from-[var(--accent-color)] to-[var(--accent-color)] bg-no-repeat bg-[length:100%_10%] bg-[0_90%] transition-[background-size] duration-250 ease-in hover:bg-[length:100%_90%]">
+            Skills
+          </span>
+        </h3>
+        <ul className="list-disc list-inside sm:text-xl mb-4">
+          <li>
+            <strong className="text-[var(--accent-color)]">Languages:</strong>{" "}
+            Java, JavaScript, Python, Kotlin (learning), Dart/Flutter (learning)
+          </li>
+          <li>
+            <strong className="text-[var(--accent-color)]">Frontend:</strong>{" "}
+            React, HTML, CSS, Tailwind, MUI
+          </li>
+          <li>
+            <strong className="text-[var(--accent-color)]">Backend:</strong>{" "}
+            Node.js, Express
+          </li>
+          <li>
+            <strong className="text-[var(--accent-color)]">
+              Databases/ORM:
+            </strong>{" "}
+            MySQL, SQLite, PostgreSQL, MongoDB, Prisma
+          </li>
+        </ul>
+        <h3 className="font-bold mb-4">
+          🌱{" "}
+          <span className="bg-gradient-to-r from-[var(--accent-color)] to-[var(--accent-color)] bg-no-repeat bg-[length:100%_10%] bg-[0_90%] transition-[background-size] duration-250 ease-in hover:bg-[length:100%_90%]">
+            Currently Learning
+          </span>
+        </h3>
+        <ul className="list-disc list-inside sm:text-xl mb-4">
+          <li>Kotlin and Android development</li>
+          <li>Flutter for cross-platform app development</li>
+        </ul>
+        <p className="text-lg mb-4">
+          📚 Always eager to learn and take on new challenges!
+        </p>
+        <h3 className="font-bold mb-4">
+          💻{" "}
+          <span className="bg-gradient-to-r from-[var(--accent-color)] to-[var(--accent-color)] bg-no-repeat bg-[length:100%_10%] bg-[0_90%] transition-[background-size] duration-250 ease-in hover:bg-[length:100%_90%]">
+            Tech Stack
+          </span>
+        </h3>
+        <h4 className="sm:text-xl font-bold mb-2 text-[var(--accent-color)]">
+          Languages
+        </h4>
+        <p className="flex flex-wrap items-center gap-2 mb-4">
+          <img
+            src="https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=openjdk&logoColor=white"
+            alt="Java"
+          />
+          <img
+            src="https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54"
+            alt="Python"
+          />
+          <img
+            src="https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E"
+            alt="JavaScript"
+          />
+          <img
+            src="https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white"
+            alt="TypeScript"
+          />
+          <img
+            src="https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white"
+            alt="HTML5"
+          />
+          <img
+            src="https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white"
+            alt="CSS3"
+          />
+
+          <img
+            src="https://img.shields.io/badge/kotlin-%237F52FF.svg?style=for-the-badge&logo=kotlin&logoColor=white"
+            alt="Kotlin"
+          />
+          <img
+            src="https://img.shields.io/badge/dart-%230175C2.svg?style=for-the-badge&logo=dart&logoColor=white"
+            alt="Dart"
+          />
+          <img
+            src="https://img.shields.io/badge/latex-%23008080.svg?style=for-the-badge&logo=latex&logoColor=white"
+            alt="LaTeX"
+          />
+          <img
+            src="https://img.shields.io/badge/markdown-%23000000.svg?style=for-the-badge&logo=markdown&logoColor=white"
+            alt="Markdown"
+          />
+        </p>
+        <h4 className="sm:text-xl font-bold mb-2 text-[var(--accent-color)]">
+          Frameworks/Libraries
+        </h4>
+        <p className="flex flex-wrap items-center gap-2 mb-4">
+          <img
+            src="https://img.shields.io/badge/daisyui-5A0EF8?style=for-the-badge&logo=daisyui&logoColor=white"
+            alt="DaisyUI"
+          />
+          <img
+            src="https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB"
+            alt="Express.js"
+          />
+          <img
+            src="https://img.shields.io/badge/FastAPI-005571?style=for-the-badge&logo=fastapi"
+            alt="FastAPI"
+          />
+          <img
+            src="https://img.shields.io/badge/Flutter-%2302569B.svg?style=for-the-badge&logo=Flutter&logoColor=white"
+            alt="Flutter"
+          />
+          <img
+            src="https://img.shields.io/badge/JWT-black?style=for-the-badge&logo=JSON%20web%20tokens"
+            alt="JWT"
+          />
+          <img
+            src="https://img.shields.io/badge/MUI-%230081CB.svg?style=for-the-badge&logo=mui&logoColor=white"
+            alt="MUI"
+          />
+          <img
+            src="https://img.shields.io/badge/NPM-%23CB3837.svg?style=for-the-badge&logo=npm&logoColor=white"
+            alt="NPM"
+          />
+          <img
+            src="https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white"
+            alt="NodeJS"
+          />
+          <img
+            src="https://img.shields.io/badge/NODEMON-%23323330.svg?style=for-the-badge&logo=nodemon&logoColor=%BBDEAD"
+            alt="Nodemon"
+          />
+          <img
+            src="https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB"
+            alt="React"
+          />
+          <img
+            src="https://img.shields.io/badge/-React%20Query-FF4154?style=for-the-badge&logo=react%20query&logoColor=white"
+            alt="React Query"
+          />
+          <img
+            src="https://img.shields.io/badge/React_Router-CA4245?style=for-the-badge&logo=react-router&logoColor=white"
+            alt="React Router"
+          />
+          <img
+            src="https://img.shields.io/badge/React%20Hook%20Form-%23EC5990.svg?style=for-the-badge&logo=reacthookform&logoColor=white"
+            alt="React Hook Form"
+          />
+          <img
+            src="https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white"
+            alt="TailwindCSS"
+          />
+          <img
+            src="https://img.shields.io/badge/vite-%23646CFF.svg?style=for-the-badge&logo=vite&logoColor=white"
+            alt="Vite"
+          />
+        </p>
+        <h4 className="sm:text-xl font-bold mb-2 text-[var(--accent-color)]">
+          Database/ORM
+        </h4>
+        <p className="flex flex-wrap items-center gap-2 mb-4">
+          <img
+            src="https://img.shields.io/badge/Prisma-3982CE?style=for-the-badge&logo=Prisma&logoColor=white"
+            alt="Prisma"
+          />
+          <img
+            src="https://img.shields.io/badge/postgres-%23316192.svg?style=for-the-badge&logo=postgresql&logoColor=white"
+            alt="Postgres"
+          />
+          <img
+            src="https://img.shields.io/badge/sqlite-%2307405e.svg?style=for-the-badge&logo=sqlite&logoColor=white"
+            alt="SQLite"
+          />
+          <img
+            src="https://img.shields.io/badge/mysql-4479A1.svg?style=for-the-badge&logo=mysql&logoColor=white"
+            alt="MySQL"
+          />
+          <img
+            src="https://img.shields.io/badge/MongoDB-%234ea94b.svg?style=for-the-badge&logo=mongodb&logoColor=white"
+            alt="MongoDB"
+          />
+        </p>
+        <h4 className="sm:text-xl font-bold mb-2 text-[var(--accent-color)]">
+          Design
+        </h4>
+        <p className="flex flex-wrap items-center gap-2 mb-4">
+          <img
+            src="https://img.shields.io/badge/Canva-%2300C4CC.svg?style=for-the-badge&logo=Canva&logoColor=white"
+            alt="Canva"
+          />
+          <img
+            src="https://img.shields.io/badge/figma-%23F24E1E.svg?style=for-the-badge&logo=figma&logoColor=white"
+            alt="Figma"
+          />
+          <img
+            src="https://img.shields.io/badge/Adobe%20Acrobat%20Reader-EC1C24.svg?style=for-the-badge&logo=Adobe%20Acrobat%20Reader&logoColor=white"
+            alt="Adobe Acrobat Reader"
+          />
+          <img
+            src="https://img.shields.io/badge/adobe%20illustrator-%23FF9A00.svg?style=for-the-badge&logo=adobe%20illustrator&logoColor=white"
+            alt="Adobe Illustrator"
+          />
+          <img
+            src="https://img.shields.io/badge/Adobe%20InDesign-49021F?style=for-the-badge&logo=adobeindesign&logoColor=FF3366"
+            alt="Adobe InDesign"
+          />
+
+          <img
+            src="https://img.shields.io/badge/Adobe%20XD-470137?style=for-the-badge&logo=Adobe%20XD&logoColor=#FF61F6"
+            alt="Adobe XD"
+          />
+          <img
+            src="https://img.shields.io/badge/Adobe%20Premiere%20Pro-9999FF.svg?style=for-the-badge&logo=Adobe%20Premiere%20Pro&logoColor=white"
+            alt="Adobe Premiere Pro"
+          />
+        </p>
+        <h4 className="sm:text-xl font-bold mb-2 text-[var(--accent-color)]">
+          CI/CD
+        </h4>
+        <p className="flex flex-wrap items-center gap-2 mb-4">
+          <img
+            src="https://img.shields.io/badge/git-%23F05033.svg?style=for-the-badge&logo=git&logoColor=white"
+            alt="Git"
+          />
+          <img
+            src="https://img.shields.io/badge/gitlab-%23181717.svg?style=for-the-badge&logo=gitlab&logoColor=white"
+            alt="GitLab"
+          />
+          <img
+            src="https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white"
+            alt="GitHub"
+          />
+        </p>
+        <h4 className="sm:text-xl font-bold mb-2 text-[var(--accent-color)]">
+          Tools
+        </h4>
+        <p className="flex flex-wrap items-center gap-2 mb-4">
+          <img
+            src="https://img.shields.io/badge/bitwarden-%23175DDC.svg?style=for-the-badge&logo=bitwarden&logoColor=white"
+            alt="Bitwarden"
+          />
+          <img
+            src="https://img.shields.io/badge/docker-%230db7ed.svg?style=for-the-badge&logo=docker&logoColor=white"
+            alt="Docker"
+          />
+          <img
+            src="https://img.shields.io/badge/ESLint-4B3263?style=for-the-badge&logo=eslint&logoColor=white"
+            alt="ESLint"
+          />
+          <img
+            src="https://img.shields.io/badge/Notion-%23000000.svg?style=for-the-badge&logo=notion&logoColor=white"
+            alt="Notion"
+          />
+          <img
+            src="https://img.shields.io/badge/Postman-FF6C37?style=for-the-badge&logo=postman&logoColor=white"
+            alt="Postman"
+          />
+          <img
+            src="https://img.shields.io/badge/prettier-%23F7B93E.svg?style=for-the-badge&logo=prettier&logoColor=black"
+            alt="Prettier"
+          />
+          <img
+            src="https://img.shields.io/badge/Trello-%23026AA7.svg?style=for-the-badge&logo=Trello&logoColor=white"
+            alt="Trello"
+          />
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default AboutPage;
